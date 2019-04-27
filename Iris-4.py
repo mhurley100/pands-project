da8b8c0099a024f34d82dc1213bffba69e87ce01
Maura Hurley <G00376337@gmit.ie>
2019-04-27 19:33:10 +0100

    Added violin pictures, removed old statistics, updated text on iris-4.py

--- Iris-4.py
+++ Iris-4.py
@@ -47,6 +47,7 @@ print(accuracy_score(Y_validation, predictions))
 print(confusion_matrix(Y_validation, predictions))
 print(classification_report(Y_validation, predictions))
 
+
 # Predict a species of Iris [ Adapted from https://pypi.org/project/tabulate/]:
 # Import the datasets
 from sklearn import neighbors, datasets
